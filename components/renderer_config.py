45a5e140e53d223baf3ad464343703f01a90cd88
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-08-03 19:13:34 +0100

    renderer_config.py cleaned up and simplified, still needs liquid params sorting.

--- components/renderer_config.py
+++ components/renderer_config.py
@@ -1,12 +1,12 @@
 import os
 
 
-class BaseConfig:
-    def __init__(self, window_size=(800, 600), shaders=None, cubemap_folder=None, camera_position=(3.2, 3.2, 3.2),
+class RendererConfig:
+    def __init__(self, window_size=(800, 600), cubemap_folder=None, camera_position=(3.2, 3.2, 3.2),
                  camera_target=(0, 0, 0), up_vector=(0, 1, 0), fov=40, near_plane=0.1, far_plane=1000,
                  light_positions=None, light_colors=None, light_strengths=None,
                  anisotropy=16.0, auto_camera=False, height_factor=1.5, distance_factor=2.0, msaa_level=8,
-                 culling=True, texture_lod_bias=0.0, env_map_lod_bias=0.0):
+                 culling=True, texture_lod_bias=0.0, env_map_lod_bias=0.0, shaders=None):
         if light_strengths is None:
             light_strengths = [0.8]
         if light_colors is None:
@@ -56,33 +56,37 @@ class BaseConfig:
     def unpack(self):
         return self.__dict__
 
+    def add_model(self, obj_path, texture_paths, shader_name='default', lod_level=7.5, rotation_speed=2000.0,
+                  rotation_axis=(0, 3, 0), apply_tone_mapping=False, apply_gamma_correction=False, width=10.0,
+                  height=10.0, **kwargs):
+        model_config = {
+            'obj_path': obj_path,
+            'texture_paths': texture_paths,
+            'shader_name': shader_name,
+            'lod_level': lod_level,
+            'rotation_speed': rotation_speed,
+            'rotation_axis': rotation_axis,
+            'apply_tone_mapping': apply_tone_mapping,
+            'apply_gamma_correction': apply_gamma_correction,
+            'width': width,
+            'height': height,
+        }
+        model_config.update(kwargs)
+        model_config.update(self.unpack())
+        return model_config
 
-class ModelConfig(BaseConfig):
-    def __init__(self, obj_path=None, texture_paths=None, shader_name='default', lod_level=7.5, rotation_speed=2000.0,
-                 rotation_axis=(0, 3, 0), apply_tone_mapping=False, apply_gamma_correction=False, width=10.0,
-                 height=10.0, **kwargs):
-        super().__init__(**kwargs)
-        self.obj_path = obj_path
-        self.texture_paths = texture_paths
-        self.shader_name = shader_name
-        self.lod_level = lod_level
-        self.rotation_speed = rotation_speed
-        self.rotation_axis = rotation_axis
-        self.apply_tone_mapping = apply_tone_mapping
-        self.apply_gamma_correction = apply_gamma_correction
-        self.width = width
-        self.height = height
-
-
-class SurfaceConfig(BaseConfig):
-    def __init__(self, shader_name='default', wave_speed=10.0, wave_amplitude=0.1, randomness=0.8,
-                 tex_coord_frequency=100.0, tex_coord_amplitude=0.1, width=500.0, height=500.0, **kwargs):
-        super().__init__(**kwargs)
-        self.shader_name = shader_name
-        self.wave_speed = wave_speed
-        self.wave_amplitude = wave_amplitude
-        self.randomness = randomness
-        self.tex_coord_frequency = tex_coord_frequency
-        self.tex_coord_amplitude = tex_coord_amplitude
-        self.width = width
-        self.height = height
+    def add_surface(self, shader_name='default', wave_speed=10.0, wave_amplitude=0.1, randomness=0.8,
+                    tex_coord_frequency=100.0, tex_coord_amplitude=0.1, width=500.0, height=500.0, **kwargs):
+        surface_config = {
+            'shader_name': shader_name,
+            'wave_speed': wave_speed,
+            'wave_amplitude': wave_amplitude,
+            'randomness': randomness,
+            'tex_coord_frequency': tex_coord_frequency,
+            'tex_coord_amplitude': tex_coord_amplitude,
+            'width': width,
+            'height': height,
+        }
+        surface_config.update(kwargs)
+        surface_config.update(self.unpack())
+        return surface_config
