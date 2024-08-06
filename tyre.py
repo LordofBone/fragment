6b8e30192409abbf2441ff8a43b6d86421b5ffc7
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-08-06 21:33:07 +0100

    calling code updated with new camera position system

--- tyre.py
+++ tyre.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     base_config = RendererConfig(
         window_size=(800, 600),
         cubemap_folder="textures/cube/mountain_lake/",
-        camera_position=(6.2, 6.2, 6.2),
+        camera_positions=[(6.2, 6.2, 6.2)],
         camera_target=(0, 0, 0),
         up_vector=(0, 1, 0),
         fov=40,
@@ -17,11 +17,9 @@ if __name__ == "__main__":
         light_strengths=[1.0],
         anisotropy=16.0,
         auto_camera=False,
-        height_factor=1.5,
-        distance_factor=2.0,
         msaa_level=8,
         culling=False,
-        texture_lod_bias=1.0,
+        texture_lod_bias=0.6,
         env_map_lod_bias=0.0,
     )
 
@@ -42,8 +40,6 @@ if __name__ == "__main__":
         rotation_axis=(0, 3, 0),
         apply_tone_mapping=False,
         apply_gamma_correction=False,
-        width=10.0,
-        height=10.0,
     )
 
     # Add the tyre renderer to the instance
