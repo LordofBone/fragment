eccff4b8a88d40c6ebcd322c43202f24f11df0c4
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-08-07 23:12:30 +0100

    Fixed weird lighting issue

--- shaders/fragment/rubber/fragment.glsl
+++ shaders/fragment/rubber/fragment.glsl
@@ -3,9 +3,6 @@
 in vec2 TexCoords;
 in vec3 FragPos;
 in vec3 Normal;
-in vec3 TangentFragPos;
-in vec3 TangentViewPos;
-in vec3 TangentLightPos[10];
 
 out vec4 FragColor;
 
@@ -14,6 +11,7 @@ uniform sampler2D normalMap;
 uniform sampler2D displacementMap;
 uniform samplerCube environmentMap;
 
+uniform vec3 lightPositions[10];
 uniform vec3 lightColors[10];
 uniform float lightStrengths[10];
 uniform float textureLodLevel;
@@ -21,6 +19,8 @@ uniform float envMapLodLevel;
 uniform bool applyToneMapping;
 uniform bool applyGammaCorrection;
 uniform float envSpecularStrength;
+
+uniform mat4 view;// Added uniform for view matrix
 
 vec3 Uncharted2Tonemap(vec3 x) {
     float A = 0.15;
@@ -41,23 +41,32 @@ vec3 toneMapping(vec3 color) {
 
 void main()
 {
+    // Retrieve normal from normal map and adjust
     vec3 normal = normalize(Normal + texture(normalMap, TexCoords, textureLodLevel).rgb * 2.0 - 1.0);
     float height = texture(displacementMap, TexCoords, textureLodLevel).r;
 
-    vec3 viewDir = normalize(TangentViewPos - TangentFragPos);
+    // Transform FragPos to view space to get view direction
+    vec4 viewFragPos = view * vec4(FragPos, 1.0);
+    vec3 viewDir = normalize(-viewFragPos.xyz);
+
+    // Calculate reflection direction
     vec3 reflectDir = reflect(viewDir, normal);
 
+    // Retrieve environment color
     vec3 envColor = textureLod(environmentMap, reflectDir, envMapLodLevel).rgb;
+
+    // Calculate ambient light
     vec3 ambient = 0.05 * texture(diffuseMap, TexCoords, textureLodLevel).rgb;
 
+    // Initialize diffuse and specular lighting
     vec3 diffuse = vec3(0.0);
     vec3 specular = vec3(0.0);
     float roughness = 0.8;
 
     for (int i = 0; i < 10; i++) {
-        vec3 lightDir = normalize(TangentLightPos[i] - TangentFragPos);
+        vec3 lightDir = normalize(lightPositions[i] - FragPos);
         float diff = max(dot(normal, lightDir), 0.0);
-        diffuse += diff * texture(diffuseMap, TexCoords, textureLodLevel).rgb * lightColors[i] * lightStrengths[i] * 0.5;
+        diffuse += diff * texture(diffuseMap, TexCoords, textureLodLevel).rgb * lightColors[i] * lightStrengths[i];
 
         vec3 halfwayDir = normalize(lightDir + viewDir);
         float spec = pow(max(dot(normal, halfwayDir), 0.0), 16.0 * (1.0 - roughness));
