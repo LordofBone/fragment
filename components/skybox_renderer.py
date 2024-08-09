cfa8e652bb0db5d2a2f5558f96a722788ed0c6ec
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-08-09 21:42:43 +0100

    skybox_renderer.py added

---
+++ components/skybox_renderer.py
@@ -0,0 +1,104 @@
+import glm
+import numpy as np
+from OpenGL.GL import *
+
+from components.abstract_renderer import AbstractRenderer, common_funcs
+
+
+class SkyboxRenderer(AbstractRenderer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.skybox_vao = None
+        self.skybox_vbo = None
+
+    def create_buffers(self):
+        """Create buffers for the skybox."""
+        vertices = [
+            -1.0, 1.0, -1.0,
+            -1.0, -1.0, -1.0,
+            1.0, -1.0, -1.0,
+            1.0, -1.0, -1.0,
+            1.0, 1.0, -1.0,
+            -1.0, 1.0, -1.0,
+
+            -1.0, -1.0, 1.0,
+            -1.0, -1.0, -1.0,
+            -1.0, 1.0, -1.0,
+            -1.0, 1.0, -1.0,
+            -1.0, 1.0, 1.0,
+            -1.0, -1.0, 1.0,
+
+            1.0, -1.0, -1.0,
+            1.0, -1.0, 1.0,
+            1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0,
+            1.0, 1.0, -1.0,
+            1.0, -1.0, -1.0,
+
+            -1.0, -1.0, 1.0,
+            -1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0,
+            1.0, -1.0, 1.0,
+            -1.0, -1.0, 1.0,
+
+            -1.0, 1.0, -1.0,
+            1.0, 1.0, -1.0,
+            1.0, 1.0, 1.0,
+            1.0, 1.0, 1.0,
+            -1.0, 1.0, 1.0,
+            -1.0, 1.0, -1.0,
+
+            -1.0, -1.0, -1.0,
+            -1.0, -1.0, 1.0,
+            1.0, -1.0, -1.0,
+            1.0, -1.0, -1.0,
+            -1.0, -1.0, 1.0,
+            1.0, -1.0, 1.0
+        ]
+
+        vertices_array = np.array(vertices, dtype=np.float32)
+
+        self.skybox_vao = glGenVertexArrays(1)
+        self.skybox_vbo = glGenBuffers(1)
+
+        glBindVertexArray(self.skybox_vao)
+
+        glBindBuffer(GL_ARRAY_BUFFER, self.skybox_vbo)
+        glBufferData(GL_ARRAY_BUFFER, vertices_array.nbytes, vertices_array, GL_STATIC_DRAW)
+
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * vertices_array.itemsize, ctypes.c_void_p(0))
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0)
+        glBindVertexArray(0)
+
+    def load_textures(self):
+        """Load the skybox cubemap."""
+        self.skybox_cubemap = glGenTextures(1)
+        if self.cubemap_folder:
+            self.load_cubemap(self.cubemap_folder, self.skybox_cubemap)
+
+    @common_funcs
+    def render(self):
+        """Render the skybox."""
+        # Use the skybox shader program
+        glUseProgram(self.shader_program)
+
+        # Set the view and projection matrices
+        view_matrix = glm.mat4(glm.mat3(self.view))  # Remove translation from the view matrix
+        projection_matrix = self.projection
+
+        glUniformMatrix4fv(glGetUniformLocation(self.shader_program, 'view'), 1, GL_FALSE, glm.value_ptr(view_matrix))
+        glUniformMatrix4fv(glGetUniformLocation(self.shader_program, 'projection'), 1, GL_FALSE,
+                           glm.value_ptr(projection_matrix))
+
+        # Bind the skybox VAO and draw it
+        glBindVertexArray(self.skybox_vao)
+        glActiveTexture(GL_TEXTURE0)
+        glBindTexture(GL_TEXTURE_CUBE_MAP, self.skybox_cubemap)
+        glUniform1i(glGetUniformLocation(self.shader_program, 'skybox'), 0)
+
+        glDrawArrays(GL_TRIANGLES, 0, 36)
+
+        glBindVertexArray(0)
