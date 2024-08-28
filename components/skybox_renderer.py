9d910c70226c1932a5f57cc60a5fcf43459aaf30
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-08-29 00:47:27 +0100

    Refactor SkyboxRenderer: Extract common buffer setup logic, implement DRY render method

--- components/skybox_renderer.py
+++ components/skybox_renderer.py
@@ -3,10 +3,6 @@ import numpy as np
 from OpenGL.GL import *
 
 from components.abstract_renderer import AbstractRenderer, common_funcs
-from components.texture_manager import TextureManager
-
-# Get the singleton instance of TextureManager
-texture_manager = TextureManager()
 
 
 class SkyboxRenderer(AbstractRenderer):
@@ -17,149 +13,54 @@ class SkyboxRenderer(AbstractRenderer):
 
     def create_buffers(self):
         """Create buffers for the skybox."""
-        vertices = [
-            -1.0,
-            1.0,
-            -1.0,
-            -1.0,
-            -1.0,
-            -1.0,
-            1.0,
-            -1.0,
-            -1.0,
-            1.0,
-            -1.0,
-            -1.0,
-            1.0,
-            1.0,
-            -1.0,
-            -1.0,
-            1.0,
-            -1.0,
-            -1.0,
-            -1.0,
-            1.0,
-            -1.0,
-            -1.0,
-            -1.0,
-            -1.0,
-            1.0,
-            -1.0,
-            -1.0,
-            1.0,
-            -1.0,
-            -1.0,
-            1.0,
-            1.0,
-            -1.0,
-            -1.0,
-            1.0,
-            1.0,
-            -1.0,
-            -1.0,
-            1.0,
-            -1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            -1.0,
-            1.0,
-            -1.0,
-            -1.0,
-            -1.0,
-            -1.0,
-            1.0,
-            -1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            -1.0,
-            1.0,
-            -1.0,
-            -1.0,
-            1.0,
-            -1.0,
-            1.0,
-            -1.0,
-            1.0,
-            1.0,
-            -1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            1.0,
-            -1.0,
-            1.0,
-            1.0,
-            -1.0,
-            1.0,
-            -1.0,
-            -1.0,
-            -1.0,
-            -1.0,
-            -1.0,
-            -1.0,
-            1.0,
-            1.0,
-            -1.0,
-            -1.0,
-            1.0,
-            -1.0,
-            -1.0,
-            -1.0,
-            -1.0,
-            1.0,
-            1.0,
-            -1.0,
-            1.0,
-        ]
-
+        vertices = self._generate_skybox_vertices()
         vertices_array = np.array(vertices, dtype=np.float32)
 
         self.skybox_vao = glGenVertexArrays(1)
         self.skybox_vbo = glGenBuffers(1)
 
         glBindVertexArray(self.skybox_vao)
+        self._setup_vertex_buffer(vertices_array)
 
+        glBindBuffer(GL_ARRAY_BUFFER, 0)
+        glBindVertexArray(0)
+
+    def _generate_skybox_vertices(self):
+        """Generate the vertex data for the skybox."""
+        return [
+            -1.0, 1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0,
+            1.0, 1.0, -1.0, -1.0, 1.0, -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, -1.0,
+            -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, -1.0,
+            -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0,
+            1.0, 1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0,
+            -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0,
+            -1.0, -1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, -1.0, -1.0, 1.0,
+        ]
+
+    def _setup_vertex_buffer(self, vertices_array):
+        """Setup the vertex buffer object."""
         glBindBuffer(GL_ARRAY_BUFFER, self.skybox_vbo)
         glBufferData(GL_ARRAY_BUFFER, vertices_array.nbytes, vertices_array, GL_STATIC_DRAW)
 
         glEnableVertexAttribArray(0)
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * vertices_array.itemsize, ctypes.c_void_p(0))
 
-        glBindBuffer(GL_ARRAY_BUFFER, 0)
-        glBindVertexArray(0)
-
     @common_funcs
     def render(self):
         glDepthFunc(
-            GL_LEQUAL
-        )  # Change depth function so depth test passes when values are equal to depth buffer's content
-        """Render the skybox."""
-        # Set the view and projection matrices
-        view_matrix = glm.mat4(glm.mat3(self.view))  # Remove translation from the view matrix
-        projection_matrix = self.projection
-
-        glUniformMatrix4fv(glGetUniformLocation(self.shader_program, "view"), 1, GL_FALSE, glm.value_ptr(view_matrix))
-        glUniformMatrix4fv(
-            glGetUniformLocation(self.shader_program, "projection"), 1, GL_FALSE, glm.value_ptr(projection_matrix)
-        )
+            GL_LEQUAL)  # Change depth function so depth test passes when values are equal to depth buffer's content
+        self._set_shader_matrices()
 
         glBindVertexArray(self.skybox_vao)
         glDrawArrays(GL_TRIANGLES, 0, 36)
         glBindVertexArray(0)
         glDepthFunc(GL_LESS)  # Set depth function back to default
+
+    def _set_shader_matrices(self):
+        """Set the view and projection matrices for the skybox shader."""
+        view_matrix = glm.mat4(glm.mat3(self.view))  # Remove translation from the view matrix
+        projection_matrix = self.projection
+
+        glUniformMatrix4fv(glGetUniformLocation(self.shader_program, "view"), 1, GL_FALSE, glm.value_ptr(view_matrix))
+        glUniformMatrix4fv(glGetUniformLocation(self.shader_program, "projection"), 1, GL_FALSE,
+                           glm.value_ptr(projection_matrix))
