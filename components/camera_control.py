c6baf89e5961129217f5c49417ab344473d1e473
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-08-29 00:33:50 +0100

    camera_control.py code made more DRY

--- components/camera_control.py
+++ components/camera_control.py
@@ -7,12 +7,8 @@ class CameraController:
         self.camera_positions = [(*pos[:3], *pos[3:]) for pos in camera_positions]
 
         # Ensure lens_rotations is a list, even if a single float is passed
-        if isinstance(lens_rotations, float) or isinstance(lens_rotations, int):
-            lens_rotations = [lens_rotations]
-        elif lens_rotations is None:
-            lens_rotations = [0.0]  # Default to no rotation if none provided
+        self.lens_rotations = self.ensure_list(lens_rotations, default=0.0)
 
-        self.lens_rotations = lens_rotations
         self.move_speed = move_speed
         self.loop = loop
         self.current_position_index = 0
@@ -20,6 +16,14 @@ class CameraController:
         self.t = 0.0
         self.current_lens_rotation_index = 0
         self.next_lens_rotation_index = 1 if len(self.lens_rotations) > 1 else 0
+
+    def ensure_list(self, value, default=0.0):
+        """Ensure that a value is a list, converting if necessary."""
+        if isinstance(value, (float, int)):
+            return [value]
+        elif value is None:
+            return [default]
+        return value
 
     def update(self, delta_time):
         self.t += self.move_speed * delta_time
@@ -30,16 +34,22 @@ class CameraController:
             self.current_lens_rotation_index = self.next_lens_rotation_index
             self.next_lens_rotation_index = (self.next_lens_rotation_index + 1) % len(self.lens_rotations)
 
-        # Interpolate between positions and rotations
-        current_pos = glm.vec3(*self.camera_positions[self.current_position_index][:3])
-        next_pos = glm.vec3(*self.camera_positions[self.next_position_index][:3])
-        interpolated_position = glm.mix(current_pos, next_pos, self.t)
-
-        current_rotation = glm.vec2(*self.camera_positions[self.current_position_index][3:])
-        next_rotation = glm.vec2(*self.camera_positions[self.next_position_index][3:])
-        interpolated_rotation = glm.mix(current_rotation, next_rotation, self.t)
+        interpolated_position = self.interpolate_positions()
+        interpolated_rotation = self.interpolate_rotations()
 
         return interpolated_position, interpolated_rotation
+
+    def interpolate_positions(self):
+        """Interpolate between current and next camera positions."""
+        current_pos = glm.vec3(*self.camera_positions[self.current_position_index][:3])
+        next_pos = glm.vec3(*self.camera_positions[self.next_position_index][:3])
+        return glm.mix(current_pos, next_pos, self.t)
+
+    def interpolate_rotations(self):
+        """Interpolate between current and next camera rotations."""
+        current_rotation = glm.vec2(*self.camera_positions[self.current_position_index][3:])
+        next_rotation = glm.vec2(*self.camera_positions[self.next_position_index][3:])
+        return glm.mix(current_rotation, next_rotation, self.t)
 
     def get_current_target(self):
         # Assuming target is always (0, 0, 0) for simplicity, this can be extended
@@ -49,8 +59,8 @@ class CameraController:
         # Interpolate between the current and next lens rotation using linear interpolation
         current_rotation = self.lens_rotations[self.current_lens_rotation_index]
         next_rotation = self.lens_rotations[self.next_lens_rotation_index]
+        return self.linear_interpolate(current_rotation, next_rotation)
 
-        # Simple linear interpolation
-        interpolated_rotation = current_rotation * (1.0 - self.t) + next_rotation * self.t
-
-        return interpolated_rotation
+    def linear_interpolate(self, start, end):
+        """Simple linear interpolation between start and end."""
+        return start * (1.0 - self.t) + end * self.t
