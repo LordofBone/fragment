325b243c42f6470785991ef7d75a47def03fd75e
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-09-04 23:40:23 +0100

    Updated frag velocity arg name

--- shaders/fragment/particles_transform_feedback_sparks/fragment.glsl
+++ shaders/fragment/particles_transform_feedback_sparks/fragment.glsl
@@ -1,14 +1,14 @@
 #version 430
 
-in vec3 frag_position;
-in vec3 frag_velocity;
+in vec3 fragPosition;
+in vec3 fragVelocity;
 
 out vec4 finalColor;
 
 void main() {
     // Set the final color of the particle
     // Use the velocity to influence color for a dynamic effect
-    vec3 color = normalize(frag_velocity) * 0.5 + 0.5;
+    vec3 color = normalize(fragVelocity) * 0.5 + 0.5;
 
     // Set the final color of the fragment
     finalColor = vec4(color, 1.0);
