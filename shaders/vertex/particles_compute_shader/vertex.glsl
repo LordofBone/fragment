cce8393bf37d54f6df300396c4e7afeaa070c1f5
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-09-15 21:06:37 +0100

    Compute shader particles vertex shader changed to remove unnecessary vars (they are now handled within the compute shader)

--- shaders/vertex/particles_compute_shader/vertex.glsl
+++ shaders/vertex/particles_compute_shader/vertex.glsl
@@ -1,29 +1,40 @@
 #version 430 core
 
-layout(location = 0) in vec3 position;
-layout(location = 1) in vec3 velocity;
-layout(location = 2) in float spawnTime;
-layout(location = 3) in float particleLifetime;
-layout(location = 4) in float particleID;
-layout(location = 5) in float lifetimePercentage;
+// Define the particle structure (same as in the compute shader)
+struct Particle {
+    vec3 position;
+    vec3 velocity;
+    float spawnTime;
+    float lifetime;
+    float particleID;
+    float lifetimePercentage;
+};
+
+// Bind the particle buffer as a shader storage buffer
+layout(std430, binding = 0) buffer ParticleBuffer {
+    Particle particles[];
+};
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
-uniform vec3 cameraPosition;
 
 out vec3 fragColor;
 
 void main() {
-    if (lifetimePercentage >= 1.0) {
+    uint index = gl_VertexID;// Use the vertex ID to access the correct particle
+
+    Particle particle = particles[index];// Fetch the particle data from the SSBO
+
+    if (particle.lifetimePercentage >= 1.0) {
         // Discard the vertex if the particle is expired
         gl_Position = vec4(0.0);
     } else {
-        vec4 worldPosition = model * vec4(position, 1.0);
+        vec4 worldPosition = model * vec4(particle.position, 1.0);
         gl_Position = projection * view * worldPosition;
 
         // Calculate color based on lifetimePercentage
-        fragColor = mix(vec3(1.0, 1.0, 1.0), vec3(1.0, 0.0, 0.0), lifetimePercentage);
+        fragColor = mix(vec3(1.0, 1.0, 1.0), vec3(1.0, 0.0, 0.0), particle.lifetimePercentage);
 
         // Set point size if using points
         gl_PointSize = 5.0;
