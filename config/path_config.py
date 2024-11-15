9cd1e6dd270f252f1c4d3448cbd41554a0e152a2
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-11-15 23:10:27 +0000

    Refactor path configurations.
    Refactored paths structure by introducing a `get_path` helper function to streamline path generation. Defined new directory paths for various assets and specific file paths for shaders, models, and textures. This enhances code maintainability and readability.

--- config/path_config.py
+++ config/path_config.py
@@ -3,13 +3,39 @@ import os
 # Get the directory of the current script
 current_dir = os.path.dirname(os.path.realpath(__file__))
 
-# Build the path to the model file
-obj_path = os.path.join(current_dir, "models", "pyramid", "pyramid.obj")
-textures_dir = os.path.join(current_dir, "textures")
-cubemaps_dir = os.path.join(textures_dir, "cubemaps")
 
-# Build the path to the vertex shader file
-vertex_shader_path = os.path.join(current_dir, "shaders", "embm", "vertex.glsl")
+# Function to build paths relative to the config directory
+def get_path(*path_parts):
+    return os.path.join(current_dir, *path_parts)
 
-# Build the path to the fragment shader file
-fragment_shader_path = os.path.join(current_dir, "shaders", "embm", "fragment.glsl")
+
+# Directories
+audio_dir = get_path("..", "audio")
+benchmarks_dir = get_path("..", "benchmarks")
+components_dir = get_path("..", "components")
+config_dir = current_dir
+gui_dir = get_path("..", "gui")
+images_dir = get_path("..", "images")
+misc_dir = get_path("..", "misc")
+models_dir = get_path("..", "models")
+screenshots_dir = get_path("..", "screenshots")
+shaders_dir = get_path("..", "shaders")
+textures_dir = get_path("..", "textures")
+themes_dir = get_path("..", "themes")
+utils_dir = get_path("..", "utils")
+
+# Specific files and subdirectories
+pyramid_obj_path = get_path("..", "models", "pyramid.obj")
+sphere_obj_path = get_path("..", "models", "sphere.obj")
+tyre_obj_path = get_path("..", "models", "tyre.obj")
+vertex_shader_dir = get_path("..", "shaders", "vertex")
+fragment_shader_dir = get_path("..", "shaders", "fragment")
+compute_shader_dir = get_path("..", "shaders", "compute")
+cubemaps_dir = get_path("..", "textures", "cube")
+diffuse_textures_dir = get_path("..", "textures", "diffuse")
+displacement_textures_dir = get_path("..", "textures", "displacement")
+normal_textures_dir = get_path("..", "textures", "normal")
+
+# Example paths for audio files
+test_audio_path = get_path("..", "audio", "music", "test.wav")
+water_pyramid_audio_path = get_path("..", "audio", "music", "water_pyramid.wav")
