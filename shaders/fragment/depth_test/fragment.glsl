87eca4cfe2013a10e1088dbc40f6127cac5c3424
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-11-23 16:23:41 +0000

    Update depth test shader to use grayscale for depth
    Changed the fragment shader to output depth values in grayscale instead of just the red channel. This allows for better visualization of depth in the rendered image. It ensures that depth information is uniformly represented across all color channels.

--- shaders/fragment/depth_test/fragment.glsl
+++ shaders/fragment/depth_test/fragment.glsl
@@ -8,5 +8,5 @@ uniform sampler2D depthMap;
 void main()
 {
     float depthValue = texture(depthMap, TexCoords).r;
-    FragColor = vec4(depthValue, 0.0, 0.0, 1.0);// Red channel represents depth
+    FragColor = vec4(depthValue, depthValue, depthValue, 1.0);// Red channel represents depth
 }
