83a8962fc5fd6c22c3b2efe226f4fedd468e4e31
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-11-23 13:35:57 +0000

    Add new depth test shaders
    Introduced new vertex and fragment shaders for depth testing. The vertex shader passes texture coordinates and computes the position, while the fragment shader samples the depth value, outputting it in the red channel. This setup helps visualize depth information in rendered scenes.

---
+++ shaders/fragment/depth_test/fragment.glsl
@@ -0,0 +1,12 @@
+#version 330 core
+
+in vec2 TexCoords;
+out vec4 FragColor;
+
+uniform sampler2D depthMap;
+
+void main()
+{
+    float depthValue = texture(depthMap, TexCoords).r;
+    FragColor = vec4(depthValue, 0.0, 0.0, 1.0);// Red channel represents depth
+}
