995920fc8322ddb4329ce940fd23abeaa0f1a0e8
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-12-01 21:00:03 +0000

    Refactor vertex shader inputs and add tangent space calculations.
    Renamed shader input variables and rearranged logic for clarity and consistency. Implemented new tangent space calculations to facilitate advanced lighting effects by transforming view direction and fragment position into tangent space. This enhances the shader's ability to accurately handle parallax mapping techniques.

--- shaders/vertex/parallax_mapping/vertex.glsl
+++ shaders/vertex/parallax_mapping/vertex.glsl
@@ -1,16 +1,16 @@
 #version 330 core
 
-layout(location = 0) in vec3 aPos;
-layout(location = 1) in vec2 aTexCoords;
-layout(location = 2) in vec3 aNormal;
-layout(location = 3) in vec3 aTangent;
-layout(location = 4) in vec3 aBitangent;
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 normal;
+layout(location = 2) in vec2 texCoords;
+layout(location = 3) in vec3 tangent;// Tangent vector
+layout(location = 4) in vec3 bitangent;// Bitangent vector
 
 out vec2 TexCoords;
 out vec3 FragPos;
 out vec3 Normal;
-out vec3 Tangent;
-out vec3 Bitangent;
+out vec3 TangentViewDir;// View direction in tangent space
+out vec3 TangentFragPos;// Fragment position in tangent space
 out vec4 FragPosLightSpace;
 
 uniform mat4 model;
@@ -20,11 +20,24 @@ uniform mat4 lightSpaceMatrix;
 
 void main()
 {
-    TexCoords = aTexCoords;
-    FragPos = vec3(model * vec4(aPos, 1.0));
-    Normal = mat3(transpose(inverse(model))) * aNormal;
-    Tangent = mat3(model) * aTangent;
-    Bitangent = mat3(model) * aBitangent;
+    FragPos = vec3(model * vec4(position, 1.0));
+    Normal = normalize(mat3(transpose(inverse(model))) * normal);
+    TexCoords = texCoords;
+
+    // Calculate tangent space matrices
+    mat3 TBN = transpose(mat3(
+    normalize(mat3(model) * tangent),
+    normalize(mat3(model) * bitangent),
+    Normal
+    ));
+
+    vec3 viewPos = vec3(inverse(view) * vec4(0.0, 0.0, 0.0, 1.0));// Camera position in world space
+    vec3 viewDir = normalize(viewPos - FragPos);
+    TangentViewDir = TBN * viewDir;
+
+    TangentFragPos = TBN * FragPos;
+
+    // Calculate position in light space
     FragPosLightSpace = lightSpaceMatrix * vec4(FragPos, 1.0);
 
     gl_Position = projection * view * vec4(FragPos, 1.0);
