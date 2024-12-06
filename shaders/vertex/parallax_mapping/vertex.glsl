0001050a3f4c596fec5c3a7b88812536aaa84147
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-12-06 23:44:55 +0000

    Simplify vertex shader by removing unused variable
    Removed the TangentFragPos variable and related TBN matrix calculation from the parallax mapping vertex shader. This change improves code clarity and performance by eliminating unnecessary computations.

--- shaders/vertex/parallax_mapping/vertex.glsl
+++ shaders/vertex/parallax_mapping/vertex.glsl
@@ -9,7 +9,6 @@ layout(location = 4) in vec3 bitangent;
 out vec2 TexCoords;
 out vec3 FragPos;
 out vec3 Normal;
-out vec3 TangentFragPos;
 out vec3 Tangent;
 out vec3 Bitangent;
 out vec4 FragPosLightSpace;
@@ -21,24 +20,21 @@ uniform mat4 lightSpaceMatrix;
 
 void main()
 {
+    // Transform vertex position to world space
     FragPos = vec3(model * vec4(position, 1.0));
+
+    // Transform normal vector to world space
     Normal = normalize(mat3(transpose(inverse(model))) * normal);
-    TexCoords = texCoords;
 
-    // Calculate tangent space matrices
-    mat3 TBN = transpose(mat3(
-    normalize(mat3(model) * tangent),
-    normalize(mat3(model) * bitangent),
-    Normal
-    ));
-
-    TangentFragPos = TBN * FragPos;
-
+    // Transform tangent and bitangent vectors to world space
     Tangent = normalize(mat3(model) * tangent);
     Bitangent = normalize(mat3(model) * bitangent);
+
+    TexCoords = texCoords;
 
     // Calculate position in light space
     FragPosLightSpace = lightSpaceMatrix * vec4(FragPos, 1.0);
 
+    // Calculate final vertex position
     gl_Position = projection * view * vec4(FragPos, 1.0);
 }
