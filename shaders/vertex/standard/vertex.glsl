cd284021ca1964a067b890356421f8d7256aa49d
314Reactor <34923578+LordofBone@users.noreply.github.com>
2024-12-20 08:35:58 +0000

    Update vertex shader to clean up unnecessary comments
    Removed redundant inline comments from the vertex shader code to improve readability and maintain consistency. This change does not affect shader functionality or pipeline behavior.

--- shaders/vertex/standard/vertex.glsl
+++ shaders/vertex/standard/vertex.glsl
@@ -3,13 +3,13 @@
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec3 normal;
 layout(location = 2) in vec2 texCoords;
-layout(location = 3) in vec3 tangent;// Provided by your new pipeline
-layout(location = 4) in vec3 bitangent;// Provided by your new pipeline
+layout(location = 3) in vec3 tangent;
+layout(location = 4) in vec3 bitangent;
 
 out vec2 TexCoords;
 out vec3 FragPos;
 out vec3 Normal;
-out mat3 TBN;// Pass TBN to fragment
+out mat3 TBN;
 
 uniform mat4 model;
 uniform mat4 view;
