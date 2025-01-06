1cb1148b8583ff4c891ba95726d9b8279b473b01
314Reactor <34923578+LordofBone@users.noreply.github.com>
2025-01-06 11:23:53 +0000

    Simplify skybox shader by replacing Lanczos sampling.
    Removed Lanczos-based cubemap sampling and replaced it with a simpler texture lookup. This improves code maintainability and reduces shader complexity.
    
    The upscalers are still under common_funcs.glsl but skybox textures are now being up-scaled offline.
    
    (Upscalers incur a high-FPS cost so this increases performance)

--- shaders/fragment/skybox/fragment.glsl
+++ shaders/fragment/skybox/fragment.glsl
@@ -6,22 +6,7 @@ out vec4 FragColor;
 
 uniform samplerCube environmentMap;
 
-// Lanczos parameters
-uniform float uOffset;// baseOffset
-uniform float uLobes;// e.g. 2.0 or 3.0
-uniform int   uSampleRadius;// e.g. 2
-uniform float uStepSize;// e.g. 0.5, 0.75, ...
-uniform float uSharpen;// e.g. 0.0 for no sharpen, 0.5 for mild, etc.
-
 void main()
 {
-    FragColor = sampleCubemapLanczos(
-    environmentMap,
-    TexCoords,
-    uOffset,
-    uLobes,
-    uSampleRadius,
-    uStepSize,
-    uSharpen
-    );
+    FragColor = texture(environmentMap, TexCoords);
 }
