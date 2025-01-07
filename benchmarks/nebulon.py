53ade052d4b96d1a1584e05b0934dc21de8dc7f4
314Reactor <34923578+LordofBone@users.noreply.github.com>
2025-01-07 10:26:58 +0000

    Fix camera position parameter order in benchmarks
    Adjusted the order of camera position parameters to ensure correctness and consistency. This change resolves potential issues with incorrect orientations during the benchmark's execution.

--- benchmarks/nebulon.py
+++ benchmarks/nebulon.py
@@ -32,20 +32,20 @@ def run_benchmark(
         duration=60,
         cubemap_folder=os.path.join(cubemaps_dir, "mountain_lake/"),
         camera_positions=[
-            (60.4, 60.4, 60.4, -50.0, 35.0),  # Starting position
-            (50.0, 70.0, 50.0, -45.0, 36.0),  # Move up and to the side
-            (40.0, 60.0, 70.0, -45.0, 36.0),  # Move further around, maintaining view
-            (30.0, 50.0, 80.0, -45.0, 36.0),  # Continue to rotate around, higher
-            (20.0, 40.0, 70.0, -45.0, 36.0),  # Move down, still rotating
-            (10.0, 30.0, 60.0, -60.0, 45.0),  # Rotate towards the back
-            (0.0, 20.0, 50.0, -45.0, -30.0),  # Directly behind, looking at the object
-            (-10.0, 30.0, 40.0, -40.0, -30.0),  # Rotate back around
-            (-20.0, 40.0, 30.0, -38.0, -23.0),  # Continue moving down
-            (-30.0, 50.0, 20.0, -45.0, -29.0),  # Directly opposite the starting point
-            (-40.0, 60.0, 30.0, -47.0, -23.0),  # Rotate around to the side
-            (-50.0, 70.0, 40.0, -48.0, -25.0),  # Move back around towards the front
-            (-60.4, 60.4, 60.4, -48.0, -25.0),  # Return to a symmetrical position opposite the start
-            (60.4, 60.4, 60.4, -45.0, 36.0),  # Return to the starting position
+            (60.4, 60.4, 60.4, 35.0, -50.0),  # Starting position
+            (50.0, 70.0, 50.0, 36.0, -45.0),  # Move up and to the side
+            (40.0, 60.0, 70.0, 36.0, -45.0),  # Move further around, maintaining view
+            (30.0, 50.0, 80.0, 36.0, -45.0),  # Continue to rotate around, higher
+            (20.0, 40.0, 70.0, 36.0, -45.0),  # Move down, still rotating
+            (10.0, 30.0, 60.0, 45.0, -60.0),  # Rotate towards the back
+            (0.0, 20.0, 50.0, -30.0, -45.0),  # Directly behind, looking at the object
+            (-10.0, 30.0, 40.0, -30.0, -40.0),  # Rotate back around
+            (-20.0, 40.0, 30.0, -23.0, -38.0),  # Continue moving down
+            (-30.0, 50.0, 20.0, -29.0, -45.0),  # Directly opposite the starting point
+            (-40.0, 60.0, 30.0, -23.0, -47.0),  # Rotate around to the side
+            (-50.0, 70.0, 40.0, -25.0, -48.0),  # Move back around towards the front
+            (-60.4, 60.4, 60.4, -25.0, -48.0),  # Return to a symmetrical position opposite the start
+            (60.4, 60.4, 60.4, 36.0, -45.0),  # Return to the starting position
         ],
         lens_rotations=[
             0.0,  # No rotation at the start
